565a53f92711a747baf02287ae6975e7cef636a8
Sophie Li <sophiemingli@gmail.com>
2016-12-21 03:32:52 -0500

    Added initial cover page

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$( document ).ready(function() {
+	$(".site-wrapper-inner").particleground({
+		dotColor: '#B1D3D1',
+		lineColor: '#B1D3D1',
+		proximity: 200,
+		parallaxMultiplier: 10
+	});
+	$(".site-wrapper-inner").particleground("pause");
+	$("#typed").typed({
+		strings: ["^600 Sophie M. Li"],
+		typeSpeed: 80,
+		onStringTyped: function() {
+			setTimeout(function(){
+				$(".fade-in").fadeTo(1500, 1);
+				$(".site-wrapper-inner").particleground("start");
+			  	$(".typed-cursor").css("visibility", "hidden");
+			}, 100);
+			
+		}
+	});
+});
